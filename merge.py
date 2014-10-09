3fb80e9f1d04bbc84e2432c78adcb8c32db63ff5
S.P. Mohanty <spmohanty91@gmail.com>
2014-10-09 20:21:20 +0530

    Adding support for 'ne' and 'namex' as etype values for NER input

--- merge.py
+++ merge.py
@@ -138,7 +138,19 @@ def merge_NER_MWE(d1,d2):
 
 	sentenceId = ner.attrib['id']
 
-	added_nerChunks = ner.xpath("//chunkNode[@attr_etype='ne']")
+	##Adding support for ne and namex as etype for NERs
+	##testing if the SSF files use ne or namex for marking NERs
+	ne_added_nerChunks = ner.xpath("//chunkNode[@attr_etype='ne']")
+	namex_added_nerChunks = ner.xpath("//chunkNode[@attr_etype='namex']")
+	added_nerChunks = []
+
+	if (len(ne_added_nerChunks)==0 and len(namex_added_nerChunks)>0):
+		added_nerChunks = namex_added_nerChunks
+	elif (len(ne_added_nerChunks)>0 and len(namex_added_nerChunks)==0):
+		added_nerChunks = ne_added_nerChunks
+	else:
+		raise NameError("Both ne and namex etypes found in the NER input file....")
+	##Added support for ne and namex as etype for NERs
 	added_mweChunks = mwe.xpath("//chunkNode[@attr_etype='mwe']")
 
 	lengthOfNodes = len(merged.xpath("//node"))
