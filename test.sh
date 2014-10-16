7ab6d79549e5c7021a942ca6f814a4f30c17b29f
S.P. Mohanty <spmohanty91@gmail.com>
2014-10-16 11:42:00 +0530

    Fixing pathnames in test.sh

--- test.sh
+++ test.sh
@@ -3,6 +3,6 @@
 for k in {1..995}
 do
         echo "File Number :: "$k
-		python merge.py --ner test_data/ltrc/ner/sentence.$k.ner.wx --mwe test_data/ltrc/mwe/sentence.$k.mwe.wx --chunker test_data/ltrc/shallowParsed/sentence.$k.shallowParse.wx        
+		python merge.py --ner tests/ltrc/ner/sentence.$k.ner.wx --mwe tests/ltrc/mwe/sentence.$k.mwe.wx --chunker tests/ltrc/shallowParsed/sentence.$k.shallowParse.wx        
 		echo "================================================================================================="
 done
